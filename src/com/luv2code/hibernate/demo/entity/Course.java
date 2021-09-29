07ac09d769d2602241f5d9498b57deebc9ed520f
Daniel Malicki <maldok31@gmail.com>
2021-09-30 00:54:14 +0200

    Initial commit

---
+++ src/com/luv2code/hibernate/demo/entity/Course.java
@@ -0,0 +1,57 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="course")
+public class Course {
+
+    //define our fields
+
+    //define constructors
+
+    //define getter setters
+
+    //define tostring
+
+    //annotate fields
+
+    private int id;
+
+    private String title;
+
+    private Instructor instructor;
+
+    public Course(){
+
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+}
---
+++ src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -0,0 +1,96 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="instructor")
+public class Instructor {
+    //annotate the class as entity ad map to db table
+
+    //define the fields
+
+    //create constructors
+
+    //annotate the fields with db column names
+
+    //create mapping
+
+    //create constructors
+
+    //genearate setter and getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    @Column(name="email")
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor_detail_id")
+    private InstructorDetail instructorDetail;
+
+    public Instructor() {
+    }
+
+    public Instructor(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public InstructorDetail getInstructorDetail() {
+        return instructorDetail;
+    }
+
+    public void setInstructorDetail(InstructorDetail instructorDetail) {
+        this.instructorDetail = instructorDetail;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", instructorDetail=" + instructorDetail +
+                '}';
+    }
+}
